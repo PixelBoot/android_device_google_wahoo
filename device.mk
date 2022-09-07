5a68082252e280e137b57ff678303f991e8110de
PixelBoot <bootingpixels@gmail.com>
2022-09-07 08:56:33 -0700

    wahoo: uprev radio aidl to 1.2

--- device.mk
+++ device.mk
@@ -263,7 +263,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Radio Hal
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.1.vendor \
+    android.hardware.radio@1.2.vendor \
     android.hardware.radio.config@1.0 \
     librmnetctl \
     libxml2
