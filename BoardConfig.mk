b73742345761a7d8fa6d581f22f6204a32c69879
PixelBoot <bootingpixels@gmail.com>
2023-11-12 14:10:06 +0100

    wahoo: Disable AVB verification

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,6 +74,9 @@ TARGET_RECOVERY_FSTAB := device/google/wahoo/fstab.hardware
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
+# Disable vbmeta checking
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
 ifneq ($(PRODUCT_INCREASE_INODE_COUNT),true)
