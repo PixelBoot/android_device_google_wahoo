6943631e87bd765efcb1d21edef478fc6282b321
PixelBoot <bootingpixels@gmail.com>
2025-04-18 23:22:13 +0200

    wahoo: Build NowPlaying service (NowPlayingOverlay)

--- device-custom.mk
+++ device-custom.mk
@@ -2,5 +2,9 @@
 PRODUCT_PACKAGES += \
     ElmyraService
 
+# NowPlayingOverlay
+ PRODUCT_PACKAGES += \
+     NowPlayingOverlay
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-custom
