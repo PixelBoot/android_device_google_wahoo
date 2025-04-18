7f95f9ac11fd714bc751a415e40ce79c5736a692
PixelBoot <bootingpixels@gmail.com>
2025-04-18 23:00:05 +0200

    Revert "wahoo: Use minimal GMS if building with it"
    This reverts commit e4a64fa1b1d0653a91541579e5cd1bb06e66f06e.

--- device-lineage.mk
+++ device-lineage.mk
@@ -2,12 +2,6 @@
 PRODUCT_PACKAGES += \
     ElmyraService
 
-# GMS
-ifeq ($(WITH_GMS),true)
-GMS_MAKEFILE=gms_minimal.mk
-WITH_GMS_COMMS_SUITE := false
-endif
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
 
